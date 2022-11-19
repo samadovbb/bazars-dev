9195444987a90d829f17c974386454aabe3e6a81
samadovbb <samadovbb@mail.ru>
2022-11-19 10:00:56 +0500

    loyality for main part

--- nodejs/main/app/user/controllers/get/program.js
+++ nodejs/main/app/user/controllers/get/program.js
@@ -1,7 +1,11 @@
 const {query} = require("../../../../database/connection/query");
 module.exports = async (req, res) => {
+    let user = await query(`select * from bazars.v_get_customers where PhoneNumber = ?;`,
+        [req.session.phoneNumber])
+
     res.render('./pages/user/program.ejs',{
-        title:"User Orders",
-        info:req.session
+        title:"User Profile",
+        info:req.session,
+        user:user[0]
     })
 }
